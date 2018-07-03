f4f32dab3af835dfa5a6dc4561dbc980290f2cd1
Your Name <acidduckling@gmail.com>
2018-07-03 21:58:09 +1000

    add Winston, Morgan and Cors

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const winston = require('winston');
 
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
+// app.use(morgan('tiny'))
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.post('/secret', (req, res) => {
   const { userInput } = req.body;
-  console.log(userInput);
+  // console.log(userInput);
   if (userInput) {
+    winston.log('info', 'user input: ' + userInput);
     res.status(200).json('success');
   } else {
+    winston.error('this guy is messing with us');
     res.status(400).json('incorrect submission');
   }
 });
