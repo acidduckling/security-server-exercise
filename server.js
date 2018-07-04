13aa31e7fbddeca6633b280e17c804f36c6ad878
Your Name <acidduckling@gmail.com>
2018-07-04 17:28:23 +1000

    whitelist cors and add secure headers

--- server.js
+++ server.js
@@ -2,16 +2,32 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const winston = require('winston');
+const helmet = require('helmet');
 
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(cors());
+const whitelist = ['http://example1.com', 'http://example2.com'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) callback(null, true);
+    else callback(new Error('Not allowed by CORS'));
+  }
+};
+
+app.use(helmet());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 // app.use(morgan('tiny'))
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  res.cookie('session', '1', { httpOnly: true, secure: true });
+  res.set({
+    'Content-Security-Policy': "script-src: 'self' 'https://apis.google.com'"
+  });
+  res.send('Hello World!');
+});
 
 app.post('/secret', (req, res) => {
   const { userInput } = req.body;
